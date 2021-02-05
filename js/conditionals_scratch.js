d1538aea3513a8ce61b8c40a4eb140feb909213f
Brett Crawford <brett.crawford57@gmail.com>
2021-02-05 16:43:23 -0600

    feat: completed exercises 1-4 plus scratch pad notes from lecture

---
+++ js/conditionals_scratch.js
@@ -0,0 +1,40 @@
+console.log("It's time for conditionals!!");
+
+// var myName = "Brett"
+//
+// if(myName === "Brett"){
+//     console.log("You should see this, I am inside the if statement ")
+// }
+//
+// var myAge = 36
+// var requiredAge = 13
+
+// if(myAge === requiredAge){
+//     console.log("You can make a social media account!")
+// }
+
+// if(myAge >= requiredAge){
+//     console.log("You can make a social media account!");
+// } else {
+//     console.log("You cannot make a social media account :-(");
+// }
+
+// var monthsSinceOilChange = 3
+//
+// var minNumberOfMonthsForOilChange = 6
+
+// if(monthsSinceOilChange <= minNumberOfMonthsForOilChange){
+//     console.log("No oil change needed at this time. Last oil change was " + monthsSinceOilChange + " months ago");
+// } else{
+//     console.log("I'm surprised your car hasn't blow up yet!");
+// }
+
+// var favColor = "red"
+//
+// if (favColor === "Red" || "red") {
+//     console.log("Hey, my favorite color is red, too!");
+// }    else if (favColor === "Blue" || "blue"){
+//     console.log("Blue is my second favorite color!")
+//     }else{
+//     console.log("I suppose " + favColor + " is ok, too")
+// }
