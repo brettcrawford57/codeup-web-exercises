72354867a32e2abe30718176b547277afa599004
Brett Crawford <brett.crawford57@gmail.com>
2021-02-12 11:37:22 -0600

    feat: started loops exercises with scratch work

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,12 +1,26 @@
-// function showMultiplicationTable(input){
-//     return
+// function showMultiplicationTable(input) {
+//
+//     for (var input = 1; input <= 10 ; input++){
+//
+//         console.log(input * 7)
+//     }
+// }
+// showMultiplicationTable()
+//
+// for( var i = 1 ; i <= 10 ; i++) {
+//     var random = Math.floor((Math.random() * 200) + 20)
+//     if (random % 2 !== 0) {
+//         console.log(random + " is odd.");
+//     } else {
+//         console.log(random + " is even.")
+//     }
 // }
 //
-// console.log('for loop iteration #');
+// for(var i = 1 ; i < 10 ; i++) {
+//     console.log(i);
+// }
 //
-
-var x = 0;
-while (x<=10){
-    x++;
-    console.log(x)
-}
+// for(var i = 100 ; i > 0 ; i = i-5){
+//     console.log(i);
+// }
+//
---
+++ js/for_loops_scratch.js
@@ -0,0 +1,59 @@
+// function showMultiplicationTable(input){
+//     return
+// }
+//
+// console.log('for loop iteration #');
+//
+
+// var x = 0;
+// while (x<=10){
+//     x++;
+//     console.log(x)
+// }
+
+// function incrementUntilTen(input) {
+//     while (input < 10){
+//         input++;
+//         console.log(input)
+//     }
+//     return console.log("Wrapped up counting!")
+// }
+// incrementUntilTen(1)
+
+
+// for (var hellomarco = 1; hellomarco <= 5; hellomarco++){
+//     console.log("You said 'Hello, Marco!' " + hellomarco + " times");
+// }
+
+// for (var i = 5; i < 1000; i = i + 5) {
+//     console.log(i)
+//     if (i === 600) {
+//         break;
+//     }
+
+// for (var i = 5; i < 1000; i++) {
+//
+//     if (i % 2 === 0) {
+//
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+for (var i = 0; i < 60; i++){
+    if (i % 6 === 0){
+        console.log("Codeup");
+    }else if (i % 2 === 0){
+        console.log("Code");
+    }else if (i % 3 === 0){
+        console.log("Up");
+    }else if (i !== 25){
+        continue;
+    }else{
+        console.log(i);
+    }
+}
+
+
+
+
