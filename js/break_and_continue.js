c6ccfa3d563e0117bfadaec3993f0080f3380658
Brett Crawford <brett.crawford57@gmail.com>
2021-02-12 14:07:45 -0600

    feat: finished 1-5 except pyramid

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,13 +1,13 @@
-while (evenNumber = prompt("Choose an odd number between 1 and 50.")){
-    if (evenNumber > 50 || evenNumber < 1) {
-        alert("I said between 1 and 50... Please try again")
-    } else if (evenNumber % 2 === 0){
-        alert("Excuse me, that's not an odd number... Please try again")
-    } else {
-        alert("Awesome! Thank you!")
-        break;
-    }
-}
+// while (evenNumber = prompt("Choose an odd number between 1 and 50.")){
+//     if (evenNumber > 50 || evenNumber < 1) {
+//         alert("I said between 1 and 50... Please try again")
+//     } else if (isNaN(evenNumber)){
+//         alert("Can you even number, bro?")
+//     } else if (evenNumber % 2 === 0){
+//         alert("Excuse me, that's not an odd number... Please try again")
+//     } else {
+//         alert("Awesome! Thank you!")
+//         break;
+//     }
+// }
 
-    // break;
-    // alert("Thank you for your cooperation!")
