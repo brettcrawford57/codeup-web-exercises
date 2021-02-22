21d898c2e562945bc78ab9534d8f1ac79fd4cb57
Brett Crawford <brett.crawford57@gmail.com>
2021-02-22 17:03:39 -0600

    feat:started arrays lecture - scratch notes

---
+++ js/arrays.js
@@ -0,0 +1,37 @@
+var pies = [];
+console.log(pies);
+
+pies = ["apple", "cherry", "key lime", "huckleberry"];
+console.log(pies);
+
+var shapes = ["square", "rectangle", "circle", "triangle"];
+console.log(shapes);
+
+var instructors = ["douglas", "kenneth", "samuel", "justin", "fernando"];
+console.log(instructors);
+
+var daysOfTheWeek = ["monday", "tuesday","wednesday","thursday","friday","saturday","sunday"];
+console.log(daysOfTheWeek);
+
+var favoriteFoods = ["pizza","tacos","enchiladas","wings","most any chicken dish","ice cream"];
+console.log(favoriteFoods);
+
+console.log("The length of shapes is " + shapes.length);
+
+console.log("The length of the instructors array is " + instructors.length);
+
+console.log("The length of the days of the week array is " + daysOfTheWeek.length);
+
+console.log("The length of the favorite foods array is " + favoriteFoods.length);
+
+console.log(shapes[0]);
+console.log(shapes[1]);
+console.log(shapes[2]);
+console.log(shapes[3]);
+console.log(shapes[4]);
+
+console.log(instructors[0]);
+console.log(instructors[1]);
+console.log(instructors[2]);
+console.log(instructors[3]);
+console.log(instructors[4]);
